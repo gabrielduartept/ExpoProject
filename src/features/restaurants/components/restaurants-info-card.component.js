f1d62e430a50d689f881dc49e9dedfad09734fdb
gabrielduartept <gabriel_ads@live.com.pt>
2022-02-10 16:58:19 -0300

    Configuracao da votacao na aplicacao

--- src/features/restaurants/components/restaurants-info-card.component.js
+++ src/features/restaurants/components/restaurants-info-card.component.js
@@ -2,8 +2,11 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import styled from 'styled-components/native';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { Card } from 'react-native-paper';
+import { SvgXml } from 'react-native-svg';
+
+import star from '../../../../assets/star';
 
 /* 
  Begin - Styling Restaurant Info Cards
@@ -18,10 +21,27 @@ const RestaurantCardCover = styled(Card.Cover)`
   backgroundColor: ${(props) => props.theme.colors.bg.primary};
 `;
 
-const Title = styled(Text)`
+const Info = styled(View)`
   padding: ${(props) => props.theme.space[3]};
+`;
+
+const Title = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
   color: ${(props) => props.theme.colors.ui.primary};
 `;
+
+const Rating = styled(View)`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const Address = styled(Text)`
+font-family: ${(props) => props.theme.fonts.body};
+font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
 
 /* 
  End - Styling Restaurant Info Cards
@@ -40,10 +60,21 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
         isClosedTemporarily,
     } = restaurant;
 
+    const ratingArray = Array.from(new Array(Math.floor(rating)));
+
     return (
         <RestaurantCard elevation={5}>
             <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-            <Title>{name}</Title>
+            <Info>
+                <Title>{name}</Title>
+                <Rating>
+                    {ratingArray.map(() => (
+                    // eslint-disable-next-line react/jsx-key
+                        <SvgXml xml={star} width={20} height={20} />
+                    ))}
+                </Rating>
+                <Address>{address}</Address>
+            </Info>
         </RestaurantCard>
     );
 };
